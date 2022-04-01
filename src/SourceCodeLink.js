a236c667cce7426192367a9ef7c347e20ab99867
robthenob <92840644+robthenob@users.noreply.github.com>
2022-03-31 21:01:31 -0700

    updated source code links

--- src/SourceCodeLink.js
+++ src/SourceCodeLink.js
@@ -6,14 +6,14 @@ export default function SourceCodeLink() {
     <div className="SourceCodeLink">
       <p>
         <a
-          href="https://github.com/robthenob/Vanilla-Weather-App"
+          href="https://github.com/robthenob/my-weather-app-react"
           target="_blank"
           rel="noreferrer"
         >
           Open source code
         </a>
         by Randi Bradley, hosted on
-        <a href="https://dreamy-dijkstra-663899.netlify.app/"> Netlify </a>
+        <a href="https://beamish-mochi-ad01b5.netlify.app/"> Netlify </a>
       </p>
     </div>
   );
